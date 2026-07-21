6fcec284c47c23ffb6cea7a586b9d099f361142f
noahpachec <noahpachec@gmail.com>
2026-07-21 14:34:40 +1000

    fixed footer resume link

--- portfolio_project/src/components/Footer.jsx
+++ portfolio_project/src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
                 <p className="lower-text">Phone Number: +61 491 628 190</p>
             </div>
 
-            <a className="upper-text" href="public/noah_resume.pdf" target="_blank" rel="noopener noreferrer" >View my resume</a>
+            <a className="upper-text" href={`${import.meta.env.BASE_URL}noah_resume.pdf`} target="_blank" rel="noopener noreferrer" >View my resume</a>
             <a className="upper-text" href="https://github.com/noahpachec" target="_blank" rel="noopener noreferrer">My GitHub</a>
             <p className="upper-text">Created by me in React</p>
 
